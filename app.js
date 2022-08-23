470d841c6a81750bc88b18ea999422c39d98c078
egii <etugsuu1217@yahoo.com>
2022-08-23 15:21:38 +0800

    Etsssin baidlaar code belen bolson

--- app.js
+++ app.js
@@ -1,13 +1,5 @@
 // toglogchiin eeljiig hadgalah huwisagch
 
-var activePlayer = 1;
-
-// Toglogchdiin tsugluulsan onoog hadgalah huwisagch
-
-var score = [0, 0]
-
-// Toglogchiin eeljindee tsugluulj bgaa onoog hadgalah huwisagch
-var roundScore = 0;
 
 // Shooni ali talaaraa buusniig hadgalah
 //Shoonii ali neg talaaraa buusniig hadgalah huwisagch heregtei 1-6  gesen утгыг энэ хувьсагчид санамсаргүйгээр үүсгэж өгнө.
@@ -23,15 +15,77 @@ var roundScore = 0;
 // document.querySelector(".dice").style.display = 'none'; // ynzan buriin
 
 // document.getElementById("score-0").textContent = 0;
+// Шоог шидсэн function
+
+var activePlayer = 0;
+// Toglogchdiin tsugluulsan onoog hadgalah huwisagch
+var score = [0, 0]
+// Toglogchiin eeljindee tsugluulj bgaa onoog hadgalah huwisagch
+var roundScore = 0;
+
 
 document.querySelector(".button").addEventListener("click", function(){
     var  diceNumber = Math.floor(Math.random() * 6) + 1;
+    document.querySelector(".dice").src = 'images/dice-' + diceNumber + '.png';
 
-    document.querySelector(".dice").src = 'images/dice-' + diceNumber + ".png"; 
-
-    // alert("Шоо буулаа:" + diceNumber);
+    if(diceNumber !== 1){ 
+        roundScore = roundScore + diceNumber;
+        document.getElementById("current-"+ activePlayer).textContent = roundScore;  
+        a = roundScore;
+    } else { 
+        b = a;
+        // console.log(a); 
+        // 1 буусан тул тоглогчийн ээлжийг энэ хэсэгт сольж өгнө
+        document.getElementById("current-"+ activePlayer).textContent = 0; 
+        // activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);  
+        if(activePlayer === 0) { 
+            score[0] = score[0] + a;
+            console.log(score[0]);
+            document.getElementById("score-0").textContent = score[0];
+            activePlayer = 1;
+            roundScore = 0;
+        } else {
+            score[1] = score[1] + a;
+            console.log(score[1]);
+            document.getElementById("score-1").textContent = score[1]; 
+            activePlayer = 0;
+            roundScore = 0;
+    }}
 });
 
+    document.querySelector(".Button2").addEventListener("click", function(){
+    
+        if(activePlayer === 0) { 
+            score[0] = score[0] + a;
+            console.log(score[0]);
+            document.getElementById("score-0").textContent = score[0];
+            document.getElementById("current-"+ activePlayer).textContent = 0; 
+            activePlayer = 1;
+        } else {
+            score[1] = score[1] + a;
+            console.log(score[1]);
+            document.getElementById("score-1").textContent = score[1]; 
+            document.getElementById("current-"+ activePlayer).textContent = 0; 
+            activePlayer = 0;
+            
+        }
+    });
+
+
+
+
+
+// shiljuulne
+// document.getElementById(score-0).textContent = 
+// if( activePlayer === 0) { 
+//     roundScore = roundScore + diceNumber;
+//     document.getElementById("score-0").textContent = roundScore;}
+// else { document.getElementById("score-1").textContent = roundScore; }
+
+// if( activePlayer === 0) { 
+//     roundScore = roundScore + diceNumber;
+//     document.getElementById("score-0").textContent = roundScore;}
+// else { document.getElementById("score-1").textContent = roundScore; } 
 // console.log("Шоо: " + diceNumber);    
 
 // function hello(){
